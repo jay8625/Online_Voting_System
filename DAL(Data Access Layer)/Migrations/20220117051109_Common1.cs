b9380d40b81f5b3d209f0e0759d9ffb122d02ba8
Jay Fegade <jaifegde2@gmail.com>
2022-01-17 11:03:07 +0530

    Identity added

---
+++ DAL(Data Access Layer)/Migrations/20220117051109_Common1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL_Data_Access_Layer_.Migrations
+{
+    public partial class Common1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
